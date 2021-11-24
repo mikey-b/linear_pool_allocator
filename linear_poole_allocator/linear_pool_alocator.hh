358f9075c0323b1e4a4ee93568fbcc72ded7c513
Michael Brown <mikey.be@gmail.com>
2021-11-24 21:14:21 +0000

    Update linear_pool_alocator.hh
    Complete Pool Allocator

--- linear_poole_allocator/linear_pool_alocator.hh
+++ linear_poole_allocator/linear_pool_alocator.hh
@@ -1,50 +1,56 @@
 #pragma once
+#include <type_traits>
+#include <cstddef>
 
-template<size_t slot_size, int pool_size = 1024*4> struct linear_pool_allocator {
+template<std::size_t slot_size, std::size_t slot_alignment = alignof(std::max_align_t), int pool_size = 1024*64> struct linear_pool_allocator {
  private:
+    using bucket_t = typename std::aligned_storage<slot_size, slot_alignment>::type;
+
     struct slot_node {
-        char* next;
+        slot_node* next;
     };
-    static_assert(sizeof(slot_node) <= slot_size);
+    static_assert(sizeof(slot_node) <= sizeof(bucket_t));
 
-    char pool[slot_size * pool_size];
-
-    char* head{ &pool[slot_size * pool_size] };
-    char** tail{ &head };
+    bucket_t* head{ &pool[pool_size] }; // This is correct! Always starts as a linear allocator and to the RIGHT of the last element
+    bucket_t** tail{ &head };
+    bucket_t pool[pool_size];
 
  public:
-    void* allocate_linear() {
+    [[gnu::malloc]] void* allocate_linear() {
         if (*tail == &pool[0]) return nullptr;
-        *tail -= slot_size;
+        *tail -= 1;
         return reinterpret_cast<void*>(*tail);
     }
 
-    void* allocate() {
+    [[gnu::malloc]] void* allocate() {
 		if (head == &pool[0]) return nullptr;
-		
+
 		if (tail == &head) {
-            head -= slot_size;
+            head -= 1;
             return reinterpret_cast<void*>(head); 
 		} else {
 			auto node = reinterpret_cast<slot_node*>(head);
-            if (tail == &node->next) tail = &head;
-			head = node->next;
-            return reinterpret_cast<void*>(node);
+            if (reinterpret_cast<slot_node**>(tail) == &node->next) {
+                tail = &head;
+			}
+			head = reinterpret_cast<bucket_t*>(node->next);
+			return reinterpret_cast<void*>(node);                
         }
+    }
 
     [[gnu::nonnull]] void deallocate(void* blk) {
-        if (*tail == reinterpret_cast<char*>(blk)) {
-            *tail += slot_size;
+        if (*tail == reinterpret_cast<bucket_t*>(blk)) {
+            *tail += 1;
         } else {
             auto node = reinterpret_cast<slot_node*>(blk);
-            node->next = head;
-            if (tail == &head) tail = &node->next;
-            head = reinterpret_cast<char*>(blk);
+            node->next = reinterpret_cast<slot_node*>(head);
+            if (tail == &head) tail = reinterpret_cast<bucket_t**>(&node->next);
+            head = reinterpret_cast<bucket_t*>(blk);
         }
     }
 
     void deallocateAll() {
-        head = &pool[slot_size * pool_size];
+        head = &pool[pool_size]; // This is correct! Always starts as a linear allocator and to the RIGHT of the last element
         tail = &head;
     }
 };
